36e44962aaac5340e95321f3d574efa6cca2fe6c
JacoRochaDev <jacofernandes@outlook.com>
2022-04-19 21:59:54 -0300

    instagram ui avançada parte 1

---
+++ app/src/main/java/com/jacorocha/instagram_r/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.jacorocha.instagram_r.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jacorocha.instagram_r.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/jacorocha/instagram_r/register/view/RegisterEmailFragment.kt
@@ -0,0 +1,18 @@
+package com.jacorocha.instagram_r.register.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jacorocha.instagram_r.R
+
+class RegisterEmailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register_email, container, false)
+    }
+}
