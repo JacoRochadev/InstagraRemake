dc16773d180cc3ab1b5b24d054703a048b0624cd
JacoRochaDev <jacofernandes@outlook.com>
2022-04-21 12:16:31 -0300

    MUDANDO A COR DA STATUS BAR E DOS ITENS DA STATUS BAR

--- app/src/main/java/com/jacorocha/instagram_r/main/view/MainActivity.kt
+++ app/src/main/java/com/jacorocha/instagram_r/main/view/MainActivity.kt
@@ -2,11 +2,16 @@ package com.jacorocha.instagram_r.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.core.content.ContextCompat
 import com.jacorocha.instagram_r.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
     }
 }
