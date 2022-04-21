abc9257d0006e1de24c6bc707fb04cd8962edbb1
JacoRochaDev <jacofernandes@outlook.com>
2022-04-21 12:02:33 -0300

    ui avançada reycler view

---
+++ app/src/main/java/com/jacorocha/instagram_r/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jacorocha.instagram_r.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jacorocha.instagram_r.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
