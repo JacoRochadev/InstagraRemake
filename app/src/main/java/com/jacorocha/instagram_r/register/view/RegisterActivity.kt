f53dece5e0cbbc08674c8599019f3116c0a400a6
JacoRochaDev <jacofernandes@outlook.com>
2022-04-28 22:50:27 -0300

    tirando fotos com a camera do smartphone

--- app/src/main/java/com/jacorocha/instagram_r/register/view/RegisterActivity.kt
+++ app/src/main/java/com/jacorocha/instagram_r/register/view/RegisterActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.jacorocha.instagram_r.R
 import com.jacorocha.instagram_r.common.view.CropperImageFragment
@@ -14,10 +17,16 @@ import com.jacorocha.instagram_r.databinding.ActivityRegisterBinding
 import com.jacorocha.instagram_r.main.view.MainActivity
 import com.jacorocha.instagram_r.register.view.RegisterNamePasswordFragment.Companion.KEY_EMAIL
 import com.jacorocha.instagram_r.register.view.RegisterWelcomeFragment.Companion.KEY_NAME
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.jvm.Throws
 
 class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var currentPhoto: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,15 +72,44 @@ class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
     }
 
     private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){ uri:  Uri? ->
-        val fragment = CropperImageFragment().apply {
-            arguments = Bundle().apply {
-                putParcelable(KEY_URI, uri)
-            }
-        }
-        replaceFragment(fragment)
+        uri?.let { openImageCropper(it) }
+
     }
     override fun goToGalleryScreen() {
         getContent.launch("image/*")
+    }
+
+    private val getCamera = registerForActivityResult(ActivityResultContracts.TakePicture()){ saved ->
+        if (saved){
+            openImageCropper(currentPhoto)
+        }
+
+    }
+
+    override fun goToCameraScreen() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager) != null){
+            val photoFile: File? = try {
+                 createImageFile()
+            }catch (e: IOException){
+                Log.e("RegisterActivity", e.message, e)
+                null
+            }
+            photoFile?.also {
+                val photoUri = FileProvider.getUriForFile(this, "com.jacorocha.instagram_r.fileprovider", it)
+                currentPhoto = photoUri
+
+                getCamera.launch(photoUri)
+            }
+
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return  File.createTempFile("JPEG_${timestamp}_", ".jpg", dir)
     }
 
     private fun replaceFragment(fragment: Fragment){
@@ -88,5 +126,14 @@ class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
                 commit()
             }
         }
+    }
+
+    private fun openImageCropper(uri: Uri){
+        val fragment = CropperImageFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(KEY_URI, uri)
+            }
+        }
+        replaceFragment(fragment)
     }
 }
